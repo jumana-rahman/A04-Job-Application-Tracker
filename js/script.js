06a9a176deae78f1c07db900850c58658a8bfff8
jumana-rahman <jumanarahman242@outlook.com>
2026-02-23 23:52:35 +0600

    toggle added in buttons

--- js/script.js
+++ js/script.js
@@ -1,10 +1,35 @@
+let interviewList = [];
+let rejectedList = [];
+
 let total = document.getElementById('total');
 let interviewCount = document.getElementById('interview-count');
 let rejectedCount = document.getElementById('rejected-count');
 
+const allFilterBtn = document.getElementById('all-filter-btn');
+const interviewFilterBtn = document.getElementById('interview-filter-btn');
+const rejectedFilterBtn = document.getElementById('rejected-filter-btn');
+
 const allCards = document.getElementById('all-cards');
+const mainContainer = document.querySelector('main');
 
 function calculateCount(){
     total.innerText = allCards.children.length;
+    interviewCount.innerText = interviewList.length;
+    rejectedCount.innerText = rejectedList.length;
 }
 calculateCount();
+
+function toggleStyle(id){
+    allFilterBtn.classList.remove('active');
+    interviewFilterBtn.classList.remove('active');
+    rejectedFilterBtn.classList.remove('active');
+
+    allFilterBtn.classList.add('bg-white', 'text-[#64748B]');
+    interviewFilterBtn.classList.add('bg-white', 'text-[#64748B]');
+    rejectedFilterBtn.classList.add('bg-white', 'text-[#64748B]');
+   
+    const selected = document.getElementById(id);
+
+    selected.classList.remove('bg-white', 'text-[#64748B]');
+    selected.classList.add('active');
+}
